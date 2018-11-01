a5db0b440f17ac538e106a0d8c5d30b9d86fedcc
Brumz <bmanrumsey24@gmail.com>
2018-11-01 11:33:19 -0600

    added style and cleaned up code

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -17,6 +17,13 @@ class AddItem extends React.Component {
           <h2>${(this.props.total / 100).toFixed(2)}</h2>
         </div>
         <form>
+          <h2>Products</h2>
+          <select
+            className="nProducts"
+            onChange={e => this.props.grabThis(e.target.value)}
+          >
+            {productList}
+          </select>
           <h2>Quantity</h2>
           <input
             type="number"
@@ -26,13 +33,6 @@ class AddItem extends React.Component {
             max="10"
             onChange={e => this.props.changeQuantity(e.target.value)}
           />
-          <h2>Products</h2>
-          <select
-            className="nProducts"
-            onChange={e => this.props.grabThis(e.target.value)}
-          >
-            {productList}
-          </select>
           <button
             type="submit"
             className="btn btn-primary"
